bb883824d863780021b2a5f37d81dc11fde96f2f
Matt Favoino <mattfavoino@gmail.com>
2017-11-21 15:30:16 -0800

    Models now able to be migrated without errors. First test passing.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -4,27 +4,28 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class ImageActiveProfile(models.Manager):
+class ProfileManager(models.Manager):
     """."""
 
     def get_queryset(self):
         """."""
-        super(ImageActiveProfile, self).get_queryset().filter(is_active=True)
+        return super(ProfileManager, self).get_queryset().filter(user__is_active=True)
 
 
 class ImagerProfile(models.Model):
     """Create user model for imager profile."""
 
-    website = models.URLField()
-    location = models.CharField(max_length=100)
-    fee = models.FloatField()
+    website = models.URLField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    fee = models.FloatField(blank=True, null=True)
     CAMERA_CHOICES = (
         ('CANON', 'Canon'),
         ('NIKON', 'Nikon'),
-        ('OLYMPUS', 'Olyumpus')
+        ('OLYMPUS', 'Olyumpus'),
+        ('FUJI', 'Fuji')
     )
     camera_choices = models.CharField(
-        max_length=2,
+        max_length=50,
         choices=CAMERA_CHOICES,
         default='CANON')
     SERVICES_CHOICES = (
@@ -33,23 +34,23 @@ class ImagerProfile(models.Model):
         ('AWKWARD', 'Awkward')
     )
     services_choices = models.CharField(
-        max_length=2,
+        max_length=50,
         choices=SERVICES_CHOICES,
         default='PORTRAIT')
     bio = models.TextField()
-    phone = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, blank=True, null=True)
     PHOTO_STYLES = (
         ('BW', 'Black and White'),
         ('Color', 'Color')
     )
     photo_styles = models.CharField(
-        max_length=2,
+        max_length=50,
         choices=PHOTO_STYLES,
         default='BW')
-    user = models.OneToOneField(User)
-    active = ImageActiveProfile()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    active = ProfileManager()
 
     @property
     def is_active(self):
-        """."""
+        """Active method for ImagerProfile."""
         return self.user.is_active
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,35 @@
+"""Test file for imager_profile app."""
 from django.test import TestCase
 
-# Create your tests here.
+import factory
+
+from imager_profile.models import ImagerProfile, User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """."""
+
+    class Meta:
+        """."""
+
+        model = User
+    username = factory.Sequence(lambda num: 'user{0}'.format(num))
+
+
+class ProfileTests(TestCase):
+    """."""
+
+    def setUp(self):
+        """."""
+        profile = ImagerProfile()
+        for num in range(20):
+            user = UserFactory.create()
+            user.set_password('pass')
+            user.save()
+        profile.user = user
+        profile.save()
+
+    def test_user_can_point_to_profile(self):
+        """."""
+        one_user = User.objects.get(id=20)
+        self.assertIsNotNone(one_user.profile)
