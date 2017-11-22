652a14cf0901a9f5467fb65f0f6b394d79dc92f8
Matt Favoino <mattfavoino@gmail.com>
2017-11-21 17:19:45 -0800

    Adding more tests.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -7,13 +7,13 @@ from imager_profile.models import ImagerProfile, User
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    """."""
+    """User factory class for use throughout tests."""
 
     class Meta:
         """."""
 
         model = User
-    username = factory.Sequence(lambda num: 'user{0}'.format(num))
+    username = 'guy1'
 
 
 class ProfileTests(TestCase):
@@ -22,14 +22,43 @@ class ProfileTests(TestCase):
     def setUp(self):
         """."""
         profile = ImagerProfile()
-        for num in range(20):
-            user = UserFactory.create()
-            user.set_password('pass')
-            user.save()
-        profile.user = user
+        self.user = UserFactory.create()
+        self.user.set_password('pass')
+        self.user.save()
+        profile.user = self.user
         profile.save()
 
     def test_user_can_point_to_profile(self):
-        """."""
-        one_user = User.objects.get(id=20)
+        """Test user can point to profile."""
+        one_user = User.objects.first()
         self.assertIsNotNone(one_user.profile)
+
+    def test_user_has_camera_choice(self):
+        """Test if user has correct default camera on profile."""
+        one_user = User.objects.get(username='guy1')
+        self.assertTrue(one_user.profile.camera_choices == 'CANON')
+
+    def test_user_has_service_choice(self):
+        """Test if user has correct default services on profile."""
+        one_user = User.objects.first()
+        self.assertTrue(one_user.profile.services_choices == 'PORTRAIT')
+
+    def test_user_has_photo_styles_choice(self):
+        """Test if user has correct default photos on profile."""
+        one_user = User.objects.first()
+        self.assertTrue(one_user.profile.photo_styles == 'BW')
+
+    def test_user_bio_is_not_none(self):
+        """Test if profile bio exists."""
+        one_user = User.objects.first()
+        self.assertIsNotNone(one_user.profile.bio)
+
+    def test_user_phone_is_empty(self):
+        """Test if profile phone is empty."""
+        one_user = User.objects.first()
+        self.assertFalse(one_user.profile.phone)
+
+    def test_user_website_is_empty(self):
+        """Test if profile website."""
+        one_user = User.objects.first()
+        self.assertFalse(one_user.profile.website)
