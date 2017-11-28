3f174ad9555f8da6354c203842cb03eae1bf2ce8
Matt Favoino <mattfavoino@gmail.com>
2017-11-27 18:58:14 -0800

    One test in imager_images

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -24,10 +24,21 @@ class PhotoTests(TestCase):
         """Setup for PhotoTests."""
         someuser = User(username='user1', password='somepassword')
         someuser.save()
-        some_pro = someuser.profile()
+        some_pro = someuser.profile
         some_pro.save()
+        album = Album(user=some_pro, title='Some Title')
+        album.save()
+        photo = Photo(user=some_pro, title='Ugly Pics')
+        photo.save()
 
     def test_user_points_to_profile(self):
         """Test if user points to a profile."""
-        one_user = User.objects.first()
-        self.assertisNotNone(one_user.profile)
+        test_user = User.objects.first()
+        self.assertIsNotNone(test_user.profile)
+
+    # def test_user_has_photo_with_title(self):
+    #     """Test if user has photo attrs."""
+    #     test_user = User.objects.first()
+    #     test_picture = test_user.profile.photo
+    #     import pdb; pdb.set_trace()
+    #     self.assertEqual(test_picture.title, 'Some Title')
